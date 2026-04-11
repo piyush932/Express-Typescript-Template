f3d0ce2c3a9f4e57d4ba609ad5d5d56e7ddab825
Piyush <b521044@iiit-bh.ac.in>
2026-04-11 10:41:55 +0530

    Added routing and controller layer

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { serverConfig } from "./config";
+import pingRouter from "./router/ping.router";
+
+const app = express();
+
+app.use(pingRouter);
+
+app.listen(serverConfig.PORT, () => {
+  console.log(`Server is running on http://localhost:${serverConfig.PORT}`);
+  console.log(`Press Ctrl+C to stop the server.`);
+});
